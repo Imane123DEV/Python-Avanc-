ec459fbae9b9b37cc1c183560bf8fcdb9ddd014d
Imane Ait-Mouh <aitmouhimane4@gmail.com>
2025-10-14 09:12:29 +0100

    Update class voiture.py

--- Devoir/class voiture.py
+++ Devoir/class voiture.py
@@ -2,18 +2,19 @@ class Voiture:
     def __init__(self, code, marque, puissance, kilometrage):
         self.code = code
         self.marque = marque
-        self.pauissance = puissance
+        self.puissance = puissance
         self.kilometrage = kilometrage
     
-    def mod_puiss(self, p):
-        self.puissance = p
+    def mod_puiss(self, puissance):
+        self.puissance = puissance
     
-    def mod_kilo(self, k):
-        self.kilometrage = k
+    def mod_kilo(self, kilometrage):
+        self.kilometrage = kilometrage
     
     def afficher(self):
         print("code:", self.code)
         print("marque:", self.marque)
         print("puissance:", self.puissance)
         print("kilometrage:", self.kilometrage)
-        
+v1=Voiture('1234','renault','90','15000')
+v1.afficher()
